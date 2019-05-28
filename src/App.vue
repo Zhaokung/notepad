e5363a9568eeb9918113be6bf619b68f94911cbf
zhaokun.long <zhaokun.long@2dm.com.au>
2019-05-28 14:46:56 +0800

    todolist

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
     export default {
         data(){
             return {
-                tools:false,
+                tools: false,
                 dialog: false,
                 table: false,
                 theme: false,
